8a1a76f8233893949860ae8d0f1269b56fda85b3
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-01-28 22:32:16 +0100

    0.1.1

--- cjs/index.js
+++ cjs/index.js
@@ -2,34 +2,37 @@
 (function (document, customElements, property) {
   var hOP = {}.hasOwnProperty;
   var $define = customElements.define;
-  property(customElements, 'define', {value: function define(
-    name,
-    Class,
-    options
-  ) {
-    var result = $define.apply(this, arguments);
-    if (!hOP.call(Class, 'new')) {
-      if (options && options.extends) {
-        var $name = options.extends;
-        var $options = {is: name};
-        property(Class, 'new', {
-          configurable: true,
-          value: function () {
-            return document.createElement($name, $options);
-          }
-        });
+  property(customElements, 'define', {
+    configurable: true,
+    value: function define(
+      name,
+      Class,
+      options
+    ) {
+      var result = $define.apply(this, arguments);
+      if (!hOP.call(Class, 'new')) {
+        if (options && options.extends) {
+          var $name = options.extends;
+          var $options = {is: name};
+          property(Class, 'new', {
+            configurable: true,
+            value: function () {
+              return document.createElement($name, $options);
+            }
+          });
+        }
+        else {
+          property(Class, 'new', {
+            configurable: true,
+            value: function () {
+              return document.createElement(name);
+            }
+          });
+        }
       }
-      else {
-        property(Class, 'new', {
-          configurable: true,
-          value: function () {
-            return document.createElement(name);
-          }
-        });
-      }
+      return result;
     }
-    return result;
-  }});
+  });
 }(
   document,
   customElements,
